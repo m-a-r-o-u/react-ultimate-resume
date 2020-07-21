a9c2b7001fa6c769476a9916543fa05a3cd7b47e
Mares.Barekzai <Mares.Barekzai@posteo.de>
2020-07-21 10:43:46 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.986040bd6b3d1b54802430217f2f340f.js"
+  "/precache-manifest.da7d2208ef6ba4975d5055f4d7b8e80d.js"
 );
 
 self.addEventListener('message', (event) => {
